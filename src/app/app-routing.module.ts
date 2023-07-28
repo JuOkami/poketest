afb4babe09c70b3a89078ca976410b670ba65b7e
JuOkami <justin.candela@gmail.com>
2023-07-28 16:39:28 +0200

    CSS et composants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PokemonsComponent} from "./composants/pokemons/pokemons.component";
+import {GenerationsComponent} from "./composants/generations/generations.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'generations', component: GenerationsComponent},
+  {path:'pokemons', component: PokemonsComponent},
+  {path:'**', component: PokemonsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
